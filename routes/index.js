25032dce206a84e7e0a207facd03899f7f4446d3
Dean Carlo Pinlac <dean.pinlac@gmail.com>
2021-06-02 11:19:34 -0400

    Add Photo for About Me

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
+let age = yearsDiff(new Date('1995-06-28'), new Date());
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Home' });
 });
@@ -13,7 +14,7 @@ router.get('/home', function (req, res, next) {
     res.render('index', { title: 'Home' });
 });
 router.get('/about', function (req, res, next) {
-    res.render('index', { title: 'About' });
+    res.render('index', { title: 'About', age: age });
 });
 router.get('/projects', function (req, res, next) {
     res.render('index', { title: 'Projects' });
@@ -24,4 +25,19 @@ router.get('/services', function (req, res, next) {
 router.get('/contact', function (req, res, next) {
     res.render('index', { title: 'Contact' });
 });
+function yearsDiff(d1, d2) {
+    let date1 = new Date(d1);
+    let date2 = new Date(d2);
+    let yearsDiff = date2.getFullYear() - date1.getFullYear();
+    if (date2.getMonth() == date1.getMonth()) {
+        if (date2.getDate() >= date1.getDate())
+            return yearsDiff;
+        else
+            return yearsDiff - 1;
+    }
+    else if (date2.getMonth() > date1.getMonth()) {
+        return yearsDiff;
+    }
+    return yearsDiff - 1;
+}
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import moment from 'moment';
 const router = express.Router();
 export default router;
+
+let age = yearsDiff(new Date('1995-06-28'), new Date());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +15,7 @@ router.get('/home', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About', age: age });
 });
 
 router.get('/projects', function(req, res, next) {
@@ -26,5 +29,23 @@ router.get('/services', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('index', { title: 'Contact' });
 });
+
+function yearsDiff(d1: Date, d2: Date) {
+  let date1 = new Date(d1);
+  let date2 = new Date(d2);
+  let yearsDiff =  date2.getFullYear() - date1.getFullYear();
+
+  if(date2.getMonth() == date1.getMonth() ){
+      if(date2.getDate() >= date1.getDate())
+          return yearsDiff;
+      else
+          return yearsDiff-1;
+  }
+  else if(date2.getMonth() > date1.getMonth()){
+      return yearsDiff;
+  }
+
+  return yearsDiff-1;
+}
 
 // module.exports = router;
