8275010b85471c929047e99bdf5440035f0fe97c
Dean Carlo Pinlac <dean.pinlac@gmail.com>
2021-06-03 23:34:34 -0400

    Refactor navigation flow and added download resume

--- routes/index.ts
+++ routes/index.ts
@@ -3,6 +3,7 @@ import moment from 'moment';
 const router = express.Router();
 export default router;
 
+//Get age of Dean Pinlac using BirthDate and Current Date
 let age = yearsDiff(new Date('1995-06-28'), new Date());
 
 /* GET home page. */
@@ -10,26 +11,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+/* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+/* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About', age: age });
 });
 
+/* GET projects page. */
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects' });
 });
 
+/* GET services page. */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services' });
 });
 
+/* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('index', { title: 'Contact' });
 });
 
+// Function for computing Difference in Years
 function yearsDiff(d1: Date, d2: Date) {
   let date1 = new Date(d1);
   let date2 = new Date(d2);
